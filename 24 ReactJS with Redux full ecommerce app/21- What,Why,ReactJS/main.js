f62e1fde2fbf8061fd50088433543501e99b90a6
shandowpro <shadisayed.6891@gmail.com>
2023-12-08 17:58:47 +0200

    new push:

--- 24 ReactJS with Redux full ecommerce app/21- What,Why,ReactJS/main.js
+++ 24 ReactJS with Redux full ecommerce app/21- What,Why,ReactJS/main.js
@@ -1,15 +1,14 @@
-//  5 Arrow Function And This Value
+//  21- What,Why,ReactJS
 /* Main lessons functions :
-    1- [This] Reference value in the Regular function  
-    2- {This} usage and attidute within in the Regular function { Auto counter application upon [this] value }
-    3- {This} usage and attidute within in the Arrow function { Auto counter application upon [this] value }
+    1-  
+     
 */
 // ---------------------------------------------------------------------
 
 // 5 Arrow Function And [This] operator  :
 function myTest() {
     'use strict' ;
-    alert('Welcome to 5 Arrow Function And This Value ') ;
+    alert('Welcome to 21- What,Why,ReactJS ') ;
     
     var dis = document.getElementById('note');
      
