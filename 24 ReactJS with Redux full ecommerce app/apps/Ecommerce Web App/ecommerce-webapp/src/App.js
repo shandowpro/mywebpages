46e322b74242d6f06dad40b17724afccc58690b9
shandowpro <shadisayed.6891@gmail.com>
2024-02-28 20:08:14 +0200

    new push webstore

---
+++ 24 ReactJS with Redux full ecommerce app/apps/Ecommerce Web App/ecommerce-webapp/src/App.js
@@ -0,0 +1,19 @@
+
+import React  from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+//  importing the  react routing  libriary :
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="text-center " >
+      this is our new project of  the web app   
+     
+    </div>
+  );
+}
+  
+export default App;
---
+++ 24 ReactJS with Redux full ecommerce app/apps/Ecommerce Web App/ecommerce-webapp/src/index.js
@@ -0,0 +1,28 @@
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// /* The following line can be included in your src/index.js or App.js file */
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import App from './App';
+ 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+  /* The following line can be included in your src/index.js or App.js file */
+
+import App from './App';
+ 
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+ 
