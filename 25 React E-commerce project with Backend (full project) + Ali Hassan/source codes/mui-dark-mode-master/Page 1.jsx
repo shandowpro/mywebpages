10d95f22885382d4f1f3fcb549b5c6de30696208
shandowpro <shadisayed.6891@gmail.com>
2024-03-03 15:14:11 +0200

    new push

---
+++ 25 React E-commerce project with Backend (full project) + Ali Hassan/source codes/mui-dark-mode-master/Page 1.jsx
@@ -0,0 +1,44 @@
+import   { useContext } from "react";
+import { ColorModeContext } from "../../theme";
+import { IconButton, useTheme } from "@mui/material";
+import { DarkModeOutlined, LightModeOutlined } from "@mui/icons-material";
+
+
+
+const Home = () => {
+  const colorMode = useContext(ColorModeContext);
+  const theme = useTheme();
+  return (
+    <div>
+      {theme.palette.mode === "light" ? (
+        <IconButton
+          onClick={() => {
+            localStorage.setItem(
+              "mode",
+              theme.palette.mode === "dark" ? "light" : "dark"
+            );
+            colorMode.toggleColorMode();
+          }}
+          color="inherit"
+        >
+          <LightModeOutlinedIcon />
+        </IconButton>
+      ) : (
+        <IconButton
+          onClick={() => {
+            localStorage.setItem(
+              "mode",
+              theme.palette.mode === "dark" ? "light" : "dark"
+            );
+            colorMode.toggleColorMode();
+          }}
+          color="inherit"
+        >
+          <DarkModeOutlinedIcon />
+        </IconButton>
+      )}
+    </div>
+  );
+};
+
+export default Home;
