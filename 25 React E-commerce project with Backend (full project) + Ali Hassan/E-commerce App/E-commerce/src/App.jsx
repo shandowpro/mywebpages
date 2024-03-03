b9ad8507b0a42e5a86774c0ea1e7c0a0ddf525f3
shandowpro <shadisayed.6891@gmail.com>
2024-03-03 16:11:05 +0200

    new phh ;

--- 25 React E-commerce project with Backend (full project) + Ali Hassan/E-commerce App/E-commerce/src/App.jsx
+++ 25 React E-commerce project with Backend (full project) + Ali Hassan/E-commerce App/E-commerce/src/App.jsx
@@ -1,12 +1,39 @@
-  
+import Header1 from "./components/header/Header1";
+import Header2 from "./components/header/Header2";
+import Header3 from "./components/header/Header3";
+
+import Typography from "@mui/material/Typography";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import { Outlet } from "react-router-dom";
+
 function App() {
- 
+  const [theme, colorMode] = useMode();
 
   return (
-    <>
-      
-    </>
-  )
+    <ColorModeContext.Provider
+      // @ts-ignore
+      value={colorMode}
+    >
+      <ThemeProvider
+        // @ts-ignore
+        theme={theme}
+      >
+        <CssBaseline />
+
+        <div>
+          <Typography variant="h3">Header</Typography>
+          <Outlet />
+
+          <h1> Hello from the main app </h1>
+
+          <Header1 />
+          <Header2 />
+          <Header3 />
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
-export default App ; 
+export default App;
