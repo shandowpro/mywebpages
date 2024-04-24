ab8d5494f41ffbbc076aa1af5bfe0eefca1af5ca
shandowpro <shadisayed.6891@gmail.com>
2024-04-24 15:13:14 +0200

    new push of portfolio

---
+++ 26 React Portfolio/React Portfolio/portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+// The main assembling file [App.jsx] of the project : 
+ 
+import './App.css'
+
+function App() {
+
+  return (
+    
+    <div> 
+      Hello this is  Shadi profoilio website   
+    </div>
+    
+  )
+}
+
+export default App ;
