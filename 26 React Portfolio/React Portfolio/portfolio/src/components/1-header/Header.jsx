82428f38c4324540e7a5c3568f8d0743859c83a7
shandowpro <shadisayed.6891@gmail.com>
2024-04-27 20:12:39 +0200

    new pysh

--- 26 React Portfolio/React Portfolio/portfolio/src/components/1-header/Header.jsx
+++ 26 React Portfolio/React Portfolio/portfolio/src/components/1-header/Header.jsx
@@ -1,15 +1,96 @@
-// First component [Header] :  
+// First component [Header] :
+import {useState} from "react";
+import "./header.css";
 
-
-
-
-import './header.css'
-
+// <div/>
 const Header = () => {
+ 
+  //  defiend a useState variable of storing the value of show modal status :  
+   const [ showModal , setshowModal ] = useState(false); 
+
+
+
   return (
-    <div>Header</div>
-  )
-}
 
-export default  Header ; 
 
+    <header className="flex">
+      <button 
+        className='menu'
+        onClick =  { () =>
+          setshowModal(true) 
+        }
+      > 
+        show modal 
+      </button>
+
+      <div />
+
+      <nav>
+        <ul className="flex">
+          <li>
+            <a href=""> About </a>
+          </li>
+
+          <li>
+            <a href=""> Articles </a>
+          </li>
+
+          <li>
+            <a href=""> Projects </a>
+          </li>
+
+          <li>
+            <a href=""> Speaking </a>
+          </li>
+
+          <li>
+            <a href=""> Contact Us </a>
+          </li>
+        </ul>
+      </nav>
+
+      <button> Light </button>
+    
+        { showModal && (
+        <div className='fixed'> 
+          <ul className='modal border'>     
+            <li >
+              <button 
+                onClick= {
+                  () => {
+                    setshowModal(false)
+                  }
+                }
+              
+              > X </button>
+            </li>
+
+            <li >
+              <a href=""> About </a>
+            </li>
+  
+            <li>
+              <a href=""> Articles </a>
+            </li>
+  
+            <li>
+              <a href=""> Projects </a>
+            </li>
+  
+            <li>
+              <a href=""> Speaking </a>
+            </li>
+  
+            <li>
+              <a href=""> Uses </a>
+            </li>
+          </ul>
+        </div>
+        )
+      }
+    
+    </header>
+  );
+};
+
+export default Header;
