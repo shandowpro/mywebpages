9b5d0dca768aa133621a5469f9ff47d41bb1499e
shandowpro <shadisayed.6891@gmail.com>
2024-04-29 17:39:13 +0200

    new push

--- 26 React Portfolio/React Portfolio/portfolio/src/components/1-header/Header.jsx
+++ 26 React Portfolio/React Portfolio/portfolio/src/components/1-header/Header.jsx
@@ -1,32 +1,25 @@
 // First component [Header] :
-import {useState} from "react";
+import { useState } from "react";
 import "./header.css";
 
 // <div/>
 const Header = () => {
- 
-  //  defiend a useState variable of storing the value of show modal status :  
-   const [ showModal , setshowModal ] = useState(false); 
-
-
+  //  defiend a useState variable of storing the value of show modal status :
+  const [showModal, setshowModal] = useState(false);
 
   return (
-
-
     <header className="flex">
+  
       <button 
-        className='menu'
-        onClick =  { () =>
-          setshowModal(true) 
-        }
+        className="menu icon-menu" 
+        onClick={() => setshowModal(true)}
       > 
-        show modal 
       </button>
 
       <div />
 
       <nav>
-        <ul className="flex">
+        <ul className="flex"> 
           <li>
             <a href=""> About </a>
           </li>
@@ -49,46 +42,46 @@ const Header = () => {
         </ul>
       </nav>
 
-      <button> Light </button>
-    
-        { showModal && (
-        <div className='fixed'> 
-          <ul className='modal border'>     
-            <li >
-              <button 
-                onClick= {
-                  () => {
-                    setshowModal(false)
-                  }
-                }
+      <button className="mode flex"> 
+        <span className ="icon-moon-o"></span> 
+      </button>
+
+      {showModal && (
+        <div className="fixed">
+          <ul className="modal border">
+            <li>
+              <button
+                onClick={() => {
+                  setshowModal(false);
+                }}
+                className="icon-clear"
+              />
               
-              > X </button>
             </li>
 
-            <li >
+            <li>
               <a href=""> About </a>
             </li>
-  
+
             <li>
               <a href=""> Articles </a>
             </li>
-  
+
             <li>
               <a href=""> Projects </a>
             </li>
-  
+
             <li>
               <a href=""> Speaking </a>
             </li>
-  
+
             <li>
               <a href=""> Uses </a>
             </li>
           </ul>
         </div>
-        )
-      }
-    
+      )}
+
     </header>
   );
 };
