60b28188f77c6879307e0d7dba093331327e41d5
shandowpro <shadisayed.6891@gmail.com>
2024-05-08 21:59:02 +0200

    new pouhs

--- 26 React Portfolio/React Portfolio/portfolio/src/components/4-contact/Contact.jsx
+++ 26 React Portfolio/React Portfolio/portfolio/src/components/4-contact/Contact.jsx
@@ -1,14 +1,44 @@
-// Fourth  component [Contact ] :  
+// Fourth  component [Contact] :
 
-import './contact.css'
+import "./contact.css";
 
-const Contact  = () => {
+const Contact = () => {
   return (
-    <div> 
-        Contact
-    </div>
-  )
-}
+    <section className="contact-us">
+      <h1 className="title">
+        <span className="icon-envelope"> </span>
+        Contact US
+      </h1>
 
-export default Contact ; 
+      <p className="sub-title">
+        {" "}
+        Contact us for more information and get notified when I push something
+        new{" "}
+      </p>
 
+      <div className="flex">
+
+        <form action="" >
+          <div className="flex">
+            <label htmlFor="email"> Email Address : </label>
+            <input type="email" name="" id="email"  required />
+          </div>
+
+          <div className="flex" style={{ marginTop:'24px' }}  >
+            <label htmlFor="message"> Your message : </label>
+
+            <textarea  required   name="" id="message">
+              
+            </textarea>
+          </div>
+
+          <button className='submit'> Submit </button>
+        </form>
+
+        <div className="animation"> Animation </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
