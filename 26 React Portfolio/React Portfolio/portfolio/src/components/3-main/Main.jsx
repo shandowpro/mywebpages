03c535b9f0612e92420f336c0c69ba931f59cd7b
shandowpro <shadisayed.6891@gmail.com>
2024-05-08 17:26:41 +0200

    jne posh

--- 26 React Portfolio/React Portfolio/portfolio/src/components/3-main/Main.jsx
+++ 26 React Portfolio/React Portfolio/portfolio/src/components/3-main/Main.jsx
@@ -1,14 +1,58 @@
-// Third component [Main] :  
+// Third component [Main] :
 
-import './main.css'
+import "./main.css";
+// import { React } from 'react';
 
-const Main  = () => {
+const Main = () => {
   return (
-    <div> 
-          Main   
-    </div>
-  )
-}
+    <main className="flex">
+      <section className="  flex left-section">
+        <button className="active">All Projects</button>
 
-export default Main ; 
+        <button>HTML & CSS</button>
 
+        <button>JavaScript</button>
+
+        <button>React & MIU</button>
+
+        <button>Node & Express</button>
+      </section>
+
+      <section className="  flex right-section">
+        
+         {["aa", "bb", "cc"  , 1 , 3  ].map((item) => {
+          return (
+            <article key={item}  className="card">
+              <img width={260} src="./1.jpg" alt="project Image" />
+
+              <div style={{ width: "260px" }} className=" box">
+                <h1 className="title"> Landing page 1 </h1>
+                <p className="sub-title">
+                  Project Card details Project Card details Project Card details
+                  Project Card details
+                </p>
+
+                <div className="flex icons ">
+                  <div style={{ gap: "11px" }} className="flex">
+                    <div className="icon-link"></div>
+                    <div className="icon-github"></div>
+                  </div>
+
+                  <a className="link flex" href="">
+                    More
+                    <span
+                      style={{ alignSelf: "center" }}
+                      className="icon-arrow-right"
+                    ></span>
+                  </a>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </section>
+    </main>
+  );
+};
+
+export default Main;
