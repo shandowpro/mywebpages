cfff6547344e225682c1340d1f3398cd19c73e21
shandowpro <shadisayed.6891@gmail.com>
2024-05-17 19:28:10 +0200

    new push

--- 26 React Portfolio/React Portfolio/portfolio/src/components/4-contact/Contact.jsx
+++ 26 React Portfolio/React Portfolio/portfolio/src/components/4-contact/Contact.jsx
@@ -1,8 +1,13 @@
 // Fourth  component [Contact] :
 
 import "./contact.css";
+import { useForm, ValidationError } from "@formspree/react";
 
 const Contact = () => {
+
+  const [state, handleSubmit] = useForm("xkndeqzg");
+
+
   return (
     <section className="contact-us">
       <h1 className="title">
@@ -17,27 +22,40 @@ const Contact = () => {
       </p>
 
       <div className="flex">
-
-        <form action="" >
+        <form onSubmit={handleSubmit} action="" className="">
           <div className="flex">
             <label htmlFor="email"> Email Address : </label>
-            <input type="email" name="" id="email"  required />
+            <input type="email" name="email" id="email"  autoComplete="off"  required />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
           </div>
 
-          <div className="flex" style={{ marginTop:'24px' }}  >
+          <div className="flex" style={{ marginTop: "24px" }}>
             <label htmlFor="message"> Your message : </label>
 
-            <textarea  required   name="" id="message">
-              
-            </textarea>
+            <textarea required name="message" id="message"></textarea>
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
           </div>
 
-          <button className='submit'> Submit </button>
+          <button className="submit" type="submit" disabled={state.submitting}>
+             {state.submitting ? 'Submitting' : 'Submit'}
+          </button>
+
+          {
+             state.succeeded &&  
+             (<p style={{'color':'red' , 'textAlign': 'center' , 'marginTop' : '1.7rem' , 'fontSize' : '25px'  }}   >Thanks for joining!</p> )
+          }
         </form>
 
         <div className="animation"> Animation </div>
       </div>
-      
     </section>
   );
 };
