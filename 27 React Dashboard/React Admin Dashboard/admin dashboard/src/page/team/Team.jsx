9693849bc3b7e6924ca6de252f4d946936330023
shandowpro <shadisayed.6891@gmail.com>
2024-06-29 17:46:11 +0200

    new push

--- 27 React Dashboard/React Admin Dashboard/admin dashboard/src/page/team/Team.jsx
+++ 27 React Dashboard/React Admin Dashboard/admin dashboard/src/page/team/Team.jsx
@@ -1,15 +1,122 @@
 
-// inner page of [Team] : 
+// inner page of  Team  :
 
-
-import React from 'react' ;
+import { DataGrid } from "@mui/x-data-grid";
+import { rows } from "./data";
+import { useTheme, Box, Typography } from "@mui/material";
+// import { red, teal, blue } from "@mui/material/colors";
+import {
+  AdminPanelSettingsOutlined,
+  LockOpenOutlined,
+  SecurityOutlined,
+} from "@mui/icons-material";
 
 const Team = () => {
-    return (    
-        <div>
-            Team page 
-        </div>
-    );
-}
 
-export default Team ;
+  // Define the useTheme Hook:
+  const theme = useTheme();
+
+  /* Define the columns public  array of object varialbe -> to assign :
+        - The number of columns in the table -> number of the elements in the array      
+        - Each column default Name  in the table ->  'field'  property value of each object -> Required & must equal the property name in value in each object the rows  
+        - Each column Custom Name  in the table ->  'headerName'  property of each object  
+        - Each column width in the table ->  'width' property of each object  
+    */
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      width: "50",
+      flex: 0.4,
+      align: "center",
+      headerAlign: "center",
+    },
+
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "phone",
+      headerName: "Phone",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "access",
+      headerName: "Access",
+      width: "100px",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+      renderCell: ({ row: { access } }) => {
+        return (
+          <Box
+            sx={{
+              p: "5px",
+              width: "99px",
+              borderRadius: "5px",
+              textAlign: "center",
+              display: "flex",
+              m: "0 auto",
+              mt: "7px",
+              justifyContent: "space-evenly",
+              backgroundColor:
+                access === "Admin"
+                  ? theme.palette.primary.dark
+                  : access === "Manager"
+                  ? theme.palette.secondary.dark
+                  : "#3da58a",
+            }}
+          >
+            {access === "Admin" && (
+              <AdminPanelSettingsOutlined
+                sx={{ color: "#fff" }}
+                fontSize="small"
+              />
+            )}
+            {access === "Manager" && (
+              <SecurityOutlined sx={{ color: "#fff" }} fontSize="small" />
+            )}
+            {access === "User" && (
+              <LockOpenOutlined sx={{ color: "#fff" }} fontSize="small" />
+            )}
+
+            <Typography sx={{ fontSize: "13px", color: "#fff" }}>
+              {access}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+  ];
+
+  return (
+    <Box sx={{ height: 600, width: "98%", mx: "auto" }}>
+      <DataGrid
+        rows={rows}
+        // @ts-ignore
+        columns={columns}
+      />
+    </Box>
+  );
+};
+
+export default Team;
---
+++ 27 React Dashboard/React Admin Dashboard/admin dashboard/src/page/team/data.jsx
@@ -0,0 +1,43 @@
+// Data of Team component :
+
+/* Define the rows public  array of object varialbe -> this  assign :
+        - The number of rows in the table -> number of the elements in the array      
+        - Each row values inside each column  in the table ->  'col1' & 'col2' & '...'  property values  of each object  
+        - Each row  id number   ->  'id' property of each object  
+    */
+export const rows = [
+  {
+    id: 1,
+    name: "Shadi Sayed",
+    email: "shadisayed@gmail.com",
+    age: "35",
+    phone: "66550498790",
+    access: "Manager",
+  },
+  {
+    id: 2,
+    name: "Osama Elzero",
+    email: "osamaelzero@gmail.com",
+    age: "43",
+    phone: "66550498790",
+    access: "Admin",
+  },
+  {
+    id: 3,
+    name: "Ali Hassan",
+    email: "alihassan@gmail.com",
+    age: "36",
+    phone: "6655568790",
+    access: "Admin",
+  },
+  {
+    id: 4,
+    name: "Ahmed Hussain",
+    email: "ahmedhussain@gmail.com",
+    age: "42",
+    phone: "56548879",
+    access: "User",
+  },
+];
+
+
