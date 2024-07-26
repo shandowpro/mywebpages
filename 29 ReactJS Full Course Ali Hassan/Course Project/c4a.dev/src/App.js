0a74f8d93fb71c6cec8685dd3ee1a6e0cff17f15
shandowpro <shadisayed.6891@gmail.com>
2024-07-27 01:28:47 +0200

    new push

---
+++ 29 ReactJS Full Course Ali Hassan/Course Project/c4a.dev/src/App.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>  The [C4A.dev] course project website </h1>    
+      <h2>  Lesson concept :  </h2>
+      <p>  This lesson about React Working mechanism , as following :     </p>
+      <ol> 
+        <li> [ index.html ] file  [the browser displayer ]  : <br></br>
+          <p> The first and main file is being run by browser    </p>
+          <p> This file include the HTML document that contains  one main contianer parent element 'div' with 'id = root'            </p>
+          <p> This  main contianer parent element 'div'  will be included by all files project , by Rendering from the [index.js] file </p>
+        </li> 
+        
+        <li> [ index.js ] file   [the react system controler] : <br></br>
+          <p> This  file include mainly on defined variable of [root] that responsible for assinging the [contianer parent element 'div' with 'id = root'] with it's extracted rendered function </p>
+          <p> Also this file include the main [CSS file] and other main components files [inlcluding the main  assembling component 'App.js' ]             </p>
+          <p> The extracted  render function of the defiend variable 'root' , contians the imported main [ assembling component 'App.js' ]        </p>
+          <p> The main imported assemgling component  [ 'App.js' ]  includes all other created and defined coponenets from other seperated files             </p>
+        </li>
+        
+        <li> [App.js] file main assembling component must be imported inside the main [index.js]  to be assinged  the deinfed root's  render function              </li> 
+      </ol>
+  
+    </div>
+  );
+}
+
+export default App;
