1c9ab1616be70cbeac16353f4ba55d8334592122
shandowpro <shadisayed.6891@gmail.com>
2024-07-29 19:17:15 +0200

    new poush

---
+++ 29 ReactJS Full Course Ali Hassan/Course Project/c4a.dev/src/pages/Lesson56.jsx
@@ -0,0 +1,73 @@
+
+import React from "react";
+
+const Lesson56 = () => {
+  return (
+    <div>
+      <h2>
+        Lesson 5 & 6 How to share the project files with other , and after
+        modifications 
+      </h2>
+      <p>
+        How to use the Github to share the project files with other public ,by
+        the next steps :
+      </p>
+      <ol>
+        <li>
+          Using github as a project repositories to upload all project files on
+          my created github account
+        </li>
+        <li>
+          There are 2 methods to use the github repos and procedures :
+          <ul>
+            <li>
+              1- From the Inner Tool of the vscode application git tool in the
+              side bar , using the next steps :
+              <ol>
+                <li>
+                  publish button - allow the githbu permission to use my github
+                  account
+                </li>
+                <li>
+                  Write repo name to be saved by as as new repo in the github
+                  account
+                </li>
+                <li>
+                  Select repo publish type from Public || Private - and choose
+                  the public incase to allow asccess and view for any visitor
+                </li>
+              </ol>
+            </li>
+
+            <li>
+              2- From the Terminal of external or inner shell of Vscode and
+              follow the default procedures
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          Download the shared project from the a Repo of github account , via
+          using the next steps :
+          <ol>
+            <li> access the shared repo form the shared link </li>
+            <li>
+              download the current project files via using code button and
+              download zip
+            </li>
+            <li> Extracting the dowlaoded folder extract here </li>
+            <li> Open with the dowlaoded folder by vscode </li>
+            <li>
+              Open terminal of inner vscode , and install first the npm of this
+              project according to the [package.json] of this project which will
+              install all dependencies of the all installed libraries
+              automatically
+            </li>
+          </ol>
+        </li>
+      </ol>
+    </div>
+  );
+};
+
+export default Lesson56;
---
+++ 29 ReactJS Full Course Ali Hassan/Course Project/c4a.dev/src/pages/Lesson7.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Lesson7 = () => {
+  return (
+    <div>
+      <h2>
+        Lesson 7 : converting HTML Websites projects into JSX react websites
+        projects
+      </h2>
+      <p>  A- Main steps of to convert the HTML project into the react JSX project , we need to
+        follow the next steps :
+      </p>
+        <ol>
+          <li> Copy the HTML elements of 'Body' main Tag - and paste inside the
+            JSX element functional component ' wether the main component or any
+            child components ' , with following considerations :
+            <ul>
+              <li>
+                All copied HTML elements must be wrapped brackets and inside a
+                parent container element inside the return of functional
+                component, to be read prooperly{" "}
+              </li>
+              <li> Must convert the class attribute into className </li>
+              <li>
+                Must convert the style='' attribute into style= and values of css
+                inside doubled curly brackets
+              </li>
+            </ul>
+          </li>
+
+          <li> Copy the HTML elements of 'Head' main Tag - and paste inside the
+            'Head' of 'index.html' of the react project , with following
+            considerations :
+            <ul>
+              <li>               
+                All copied HTML elements must be direct link path  
+              </li>     
+            </ul>
+          </li>
+          
+          <li>  Copy the CSS files 'from HTML old project folder '   and put them inside the   ' react project folder' beside the 'index.html'  inside the public    </li>
+        </ol>
+
+
+        <p> B- HTML codes into JSX converting methods : 
+          <ul>
+            <li> Manualy method    </li>
+            <li> Shorten method :
+              <p> By using one of the online converting websites   </p>
+              <p> This method only including converting elements and script , but  not include  the file  arrangment              </p>             
+            </li>          
+          </ul>
+        
+        
+        </p>
+    </div>
+  );
+};
+
+export default Lesson7;
