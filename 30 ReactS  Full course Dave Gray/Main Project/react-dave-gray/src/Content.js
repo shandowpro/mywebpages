9741a08b293aaacea9610443c2da1f11e6eae8d4
shandowpro <shadisayed.6891@gmail.com>
2024-08-24 23:08:51 +0200

    new push

--- 30 ReactS  Full course Dave Gray/Main Project/react-dave-gray/src/Content.js
+++ 30 ReactS  Full course Dave Gray/Main Project/react-dave-gray/src/Content.js
@@ -17,7 +17,7 @@
 
 */
 
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 // Importing the  lessons components :
 import Lesson7 from "./Lesson7";
@@ -26,32 +26,79 @@ import Lesson9 from "./Lesson9";
 import ColorChangerApp from "./ColorChangerApp";
 
 const Content = () => {
-   
+  //  Define a varible of the [API url] to deal with it using [fetch()]  :
+  const API_URL = "http://localhost:3500/items";
+
   // a- Define state of the main list items to be used as main [datasource] OR an empty array {when teh localstorage array is being deleted by the user or for the firt visitor   }  as a dynamic data from the localStorage (instead of using static array ) :
-  const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("shoppinglist")) || []
-  );
+  const [items, setItems] = useState([]);
+  // JSON.parse(localStorage.getItem("shoppinglist")) ||
 
-  // b- Define useEffect function to be used in executing codes for the first only of [(saving for the first time) the defined state [items] after being changed into the  local storage ] according to the dependency  of {items}  ->   : 
-  useEffect(()=> {
-    // Adding the new value of the [items] list into the local storage of browser each time the itmes is being  changed :
-    localStorage.setItem("shoppinglist", JSON.stringify(items));
-  } , [items] )
-//  ---------------------------------------
+  // b- Define a new state variable of the fetched error :
+  const [fetchError, setFetchError] = useState(null);
 
-  // c- Define a state variable for  search fuctionality  :
+  // c- Define a new state variable of isLoading message  :
+  const [isLoading , setIsLoading] = useState(true);
+
+  // d- Define a state variable for  search fuctionality  :
   const [search, setSearch] = useState("");
   // -------------------------------------------
 
-  // d-  Define a state variable to be used for storing the new item as paramter inside each functions [  [handleClick] & [handleDelete]   -] :
+  // e-  Define a state variable to be used for storing the new item as paramter inside each functions [  [handleClick] & [handleDelete]   -] :
   const [newItem, setNewItem] = useState("");
   // ------------------------------------------------------------------
 
+  // b- Define useEffect function to be used in executing codes for the first loading only []  ->   :
+  useEffect(() => {
+    // Define a fuction of fetching for data from given api => [to be used only for the first load ]   :
+    const fetchItems = async () => {
+      try {
+        // 1- Define a variable of awaited fetching from the assinged given [ API_URL] :
+        const response = await fetch(API_URL);
+
+        // 2- Checking if there are an error of fetching response [error of not getting data] -> before the reciving data inside the [listItems]  => this code will be executed not the [catch error] :
+        if (!response.ok) throw Error("Did not receive expected data ");
+
+        // Define a variable of the responsed data after converting into  a json format           :
+        const listItems = await response.json();
+
+        // Setting the obtained json data into the defiend state variable [usgin the setter function ]    :
+        setItems(listItems);
+
+        // test printing of the obtainwed data from api json server  :
+        // console.log(listItems);
+
+        // Handling the custom error message using the defined error state [setFetchError()] to set 'null' value instead of the Current  obtained :
+        setFetchError(null);
+      } catch (err) {
+        //  Printing the error stack [this error will not be dispalehed due to not reached the defined response of data ] :
+        // console.log(err.stack) ;
+        setFetchError(err.message);
+      } finally {
+        setIsLoading(false) ;
+      } 
+    };
+
+    // Call the defined previous function of [fetchItems()] directly =>  to be called inside the {useEffect} hook => calling the async [fetchItmes] insde the {setTimer()}  to simulate the slow  loading api and show the loading message :
+    setTimeout(()=> {
+      (async () => await fetchItems())();
+    }, 3000 )
+
+  }, []);
+
+  // b- Define useEffect function to be used in executing codes for the first only of [(saving for the first time) the defined state [items] after being changed into the  local storage ] according to the dependency  of {items}  ->   :
+  // useEffect(()=> {
+  //   // Adding the new value of the [items] list into the local storage of browser each time the itmes is being  changed :
+  //   localStorage.setItem("shoppinglist", JSON.stringify(items));
+  // } , [items] )
+
+  //  ---------------------------------------
+
+
   // Define a identical pattern of each function to be used inside both   [handleClick , deleteClick]  :
   // const setAndSaveItems = (newItems) => {
-    // Setting the value of the paramter inside the dataSource state  itemslist  array using its setter function :
-    // setItems(newItems);
-    // localStorage.setItem("shoppinglist", JSON.stringify(newItems));
+  // Setting the value of the paramter inside the dataSource state  itemslist  array using its setter function :
+  // setItems(newItems);
+  // localStorage.setItem("shoppinglist", JSON.stringify(newItems));
   // };
 
   // b- [Switching Clicking of item list ] =>  Define Handling click function to handle the clicked input [more dynamically] :
@@ -61,9 +108,8 @@ const Content = () => {
       item.id === id ? { ...item, checked: !item.checked } : item
     );
 
-    // Adding the new value of the  [list items] into the defined  state of [items] after being updated (change checked property after checking an input ) : 
+    // Adding the new value of the  [list items] into the defined  state of [items] after being updated (change checked property after checking an input ) :
     setItems(listItems);
-     
   };
   // ------------------------------------------------------------
 
@@ -72,7 +118,7 @@ const Content = () => {
     // 1- Creating a new array of item - of non selected items - to be stored as new array of  items :
     const listItems = items.filter((item) => item.id !== id);
 
-    // Adding the new value of the  [list items] into the defined state of [items] after being updated ( remove an input form the list  ) : 
+    // Adding the new value of the  [list items] into the defined state of [items] after being updated ( remove an input form the list  ) :
     setItems(listItems);
 
     // calling idetcial function by using the (listItems) parameter  :
@@ -113,28 +159,33 @@ const Content = () => {
     setNewItem("");
   };
   //   -------------------------------------------
-
+  
   return (
     <main className="App-header" style={{ margin: "10px" }}>
-      <h1> Content Component </h1>
-
+    <h1> Content Component </h1>
       <Lesson56 />
-
-      <Lesson7
-        title="Listing and keys"
-        subTitle="Listing and keys"
-        items={items.filter((item) =>
-          item.item.toLowerCase().includes(search.toLowerCase())
-        )}
-        newItem={newItem}
-        setItems={setItems}
-        setNewItem={setNewItem}
-        handleClick={handleClick}
-        handleDelete={handleDelete}
-        handleSubmit={handleSubmit}
-        search={search}
-        setSearch={setSearch}
-      />
+      { 
+        fetchError ? (
+          <p style={{ color:"red"}}> {`Lesson789: Error: ${fetchError}` } </p>
+        ) : ( isLoading ? ( <p style={{ color:"green" , fontSize:'30px' }}  > Loading data...  </p> ) : (
+          <Lesson7
+            title="Listing and keys"
+            subTitle="Listing and keys"
+            items={items.filter((item) =>
+              item.item.toLowerCase().includes(search.toLowerCase())
+            )}
+            newItem={newItem}
+            setItems={setItems}
+            setNewItem={setNewItem}
+            handleClick={handleClick}
+            handleDelete={handleDelete}
+            handleSubmit={handleSubmit}
+            search={search}
+            setSearch={setSearch}
+          />
+          )
+        )   
+      } 
 
       <Lesson9
         title="Lesson9"
@@ -142,9 +193,7 @@ const Content = () => {
         count={items.length}
       />
 
-
       <ColorChangerApp />
-      
     </main>
   );
 };
